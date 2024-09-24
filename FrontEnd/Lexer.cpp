8b88d906e8af4678f976d05805a3d466ecb25286
Alexander Cooper <alexanderecooper@gmail.com>
2024-09-24 09:14:40 -0700

    save work

--- FrontEnd/Lexer.cpp
+++ FrontEnd/Lexer.cpp
@@ -1,6 +1,114 @@
+#include <cassert>
 #include "Lexer.hpp"
 
 void Lexer::lex()
 {
-    
+    // TODO handle the rest of the tokens later, like floats
+    while (!eof())
+    {
+        if (isalpha()) 
+        {
+            // attempt to lex a keyword
+            std::optional<std::unique_ptr<Token>> token = attempt_lex_keyword();
+            if (token)
+            {
+                tokens.push_back(token.value());
+                continue;
+            }
+        }
+
+        // TODO handle comments
+
+        if (iswhitespace()) 
+        {
+            advance();
+        }
+        else if (isdigit())
+        {
+            auto token = lex_integer();
+            tokens.push_back(token);
+        }
+        else if (isid() && !isdigit())
+        {
+            auto token = lex_id();
+            tokens.push_back(token);
+        }
+        else if (issep())
+        {
+            auto token = lex_sep();
+            tokens.push_back(token);
+        }
+        else if (isop())
+        {
+            auto token = lex_op();
+            tokens.push_back(token);
+        }
+    }
+}
+
+std::optional<std::unique_ptr<Token>> Lexer::attempt_lex_keyword()
+{
+    for (auto keyword : keywords) 
+    {
+        if (index + keyword.length() < input.length())
+        {
+            if (slice(keyword.length()) == keyword)
+            {
+                advance(keyword.length());
+                return std::make_unique<Token>(TokenType::Keyword, keyword);
+            }
+        }
+    }
+
+    return {};
+}
+
+std::unique_ptr<Token> Lexer::lex_integer()
+{
+    std::string value = "";
+    while (!eof() && isdigit())
+    {
+        value += current();
+        advance();
+    }
+
+    return std::make_unique<Token>(Token(TokenType::Integer, value));
+}
+
+std::unique_ptr<Token> Lexer::lex_id()
+{
+    assert(!isdigit() && "Lexer is trying to parse an id that starts with an integer");
+
+    std::string value = "";
+    while (!eof() && isid())
+    {
+        value += current();
+        advance();
+    }
+
+    return std::make_unique<Token>(Token(TokenType::Id, value));
+}
+
+std::unique_ptr<Token> Lexer::lex_sep()
+{
+    std::string value = "";
+    while (!eof() && issep())
+    {
+        value += current();
+        advance();
+    }
+
+    return std::make_unique<Token>(Token(TokenType::Sep, value));
+}
+
+std::unique_ptr<Token> Lexer::lex_op()
+{
+    std::string value = "";
+    while (!eof() && isop())
+    {
+        value += current();
+        advance();
+    }
+
+    return std::make_unique<Token>(Token(TokenType::Op, value));
 }
--- FrontEnd/Lexer.hpp
+++ FrontEnd/Lexer.hpp
@@ -2,21 +2,71 @@
 
 #include <string>
 #include <vector>
+#include <optional>
 #include <memory>
 
 enum class TokenType 
 {
+    Integer,
     Id,
+    Keyword,
     Op,
-    Sep,
-    Type
+    Sep
 };
 
 struct Token
 {
     TokenType type;
     std::string value;
+
+    Token(const TokenType type, const std::string& value) : type(type), value(value) {}
 };
+
+std::vector<std::string> seperators = {
+    ";",
+    ",",
+    "(",
+    ")",
+    "{",
+    "}",
+    "[",
+    "]"
+};
+
+std::vector<std::string> operators = {
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    "&",
+    "++",
+    "--",
+    "+=",
+    "-=",
+    "*=",
+    "/=",
+    "%="
+};
+
+std::vector<std::string> keywords = {
+    "void",
+    "int", // TODO add more later
+    "return"
+};
+
+bool vector_contains_char(std::vector<std::string>&, char c)
+{
+    for (auto sep : seperators)
+    {
+        if (sep.find(c) != std::string::npos) 
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 class Lexer
 {
@@ -26,10 +76,28 @@ private:
     std::vector<std::unique_ptr<Token>> tokens;
 
     void lex();
+
+    std::optional<std::unique_ptr<Token>> attempt_lex_keyword();
+    std::unique_ptr<Token> lex_integer();
+    std::unique_ptr<Token> lex_id();
+    std::unique_ptr<Token> Lexer::lex_sep();
+    std::unique_ptr<Token> Lexer::lex_op();
+
     bool eof() const { return index >= input.length(); }
     char current() const { return input[index]; }
     std::string slice(int length) { return input.substr(index, length); }
-    bool whitespace() const { return std::isspace(current()); }
+    void advance() { index++; }
+    void advance(int i) { index += i; }
+
+    bool iswhitespace() const { return std::isspace(current()); }
+    bool isdigit() const { return std::isdigit(current()); }
+    bool isfloat() const { return isdigit() || current() == '.'; }
+    bool isalpha() const { return std::isalpha(current()); } 
+    bool isalnum() const { return std::isalnum(current()); }
+    bool isid() const { return isalnum() || current() == '_'; }
+    bool issep() const { return vector_contains_char(seperators, current()); }
+    bool isop() const { return vector_contains_char(operators, current()); }
+
 public:
     Lexer(const std::string& input) : input(input) {}
 
