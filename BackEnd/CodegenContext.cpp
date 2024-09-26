6aee37723b35e9e39825621aed8e53608124bd57
Alexander Cooper <alexanderecooper@gmail.com>
2024-09-26 13:25:23 -0700

    save

--- BackEnd/CodegenContext.cpp
+++ BackEnd/CodegenContext.cpp
@@ -1,1 +1,8 @@
 #include "CodegenContext.hpp"
+
+CodegenContext::CodegenContext()
+{
+    llvm_context = std::make_unique<llvm::LLVMContext>();
+    llvm_builder = std::make_unique<llvm::IRBuilder<>>(*llvm_context);
+    llvm_module = std::make_unique<llvm::Module>("main_module", *llvm_context);
+}
--- BackEnd/CodegenContext.hpp
+++ BackEnd/CodegenContext.hpp
@@ -2,9 +2,17 @@
 
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
 #include <llvm/IR/Value.h>
+#include <memory>
+#include <map>
 
 struct CodegenContext
 {
-    
+    std::unique_ptr<llvm::LLVMContext> llvm_context;
+    std::unique_ptr<llvm::IRBuilder<>> llvm_builder;
+    std::unique_ptr<llvm::Module> llvm_module;
+    std::map<std::string, llvm::Value *> named_values;
+
+    CodegenContext();
 };
